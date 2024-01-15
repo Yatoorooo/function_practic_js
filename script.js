dfb8b21422245e71eeb76f963184c124eb1a852b
Yatoorooo <Egvarakcin@gmail.com>
2024-01-16 00:09:20 +0300

    two commit

--- script.js
+++ script.js
@@ -5,8 +5,13 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, item){
-   array.splice(4, 1);
-   return array;
+   let number = array.indexOf(item);
+   if(number !== -1){
+      array.splice(number, 1);
+
+   };
 };
-console.log(removeElement(array));
+removeElement(array, 5);
+console.log(array);
+
 // Результат: [1, 2, 3, 4, 6, 7]
